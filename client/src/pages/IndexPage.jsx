0c4c31a7371d8b9203fa819b8cf2a0e4051f0c85
James DeFoggia <jdefoggia@gmail.com>
2024-07-04 12:09:46 -0400

    added other sections to index page, services, products

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,22 +1,31 @@
 // import Scene from "../components/canvas/Scene";
 
 import Location from "../components/Location";
+import Products from "../components/Products";
+import Services from "../components/Services";
+
 
 export default function IndexPage() {
     return (
         <div>
-            <div className="flex flex-col items-center justify-center min-h-screen"> {/* Adjusted to make it a flex container centered in both axes */}
-                <div className='container bg-mblue rounded-lg shadow-md shadow-lbrown flex flex-col items-center justify-normal mb-4 pb-4 mx-auto w-full max-w-4xl'> {/* Added mx-auto and max-w-4xl for responsive width */}
+            <div className="flex flex-col items-center justify-center min-h-screen">
+                <div className='index-container'>
                     <h1 className='text-6xl font-bold font-dscript text-center mt-4 text-lbrown'>BBronze Mobile Spray Tanning</h1>
                     <p className='text-lg text-mbrown font-asap'>Premium Mobile Tanning Sevice in the North Pittsburgh area.</p>
                 </div>
-                <div className="container bg-mblue rounded-lg shadow-md shadow-lbrown flex flex-col items-center justify-normal mt-4 mb-4 pb-4 pt-4 mx-auto w-full max-w-4xl"> {/* Added mx-auto and max-w-4xl for responsive width */}
+                <div className="index-container">
+                    <Services />
+                </div>
+                <div className="index-container">
                     <h1 className='text-5xl font-bold font-asap text-center mt-4 mb-4 underline text-lbrown'>Location</h1>
                     <h2 className='text-2xl font-bold font-mplus text-center mt-4 mb-4 text-lbrown'>Below is the area we service. Closer location means no service fee, farther is a small fee.</h2>
                     <Location />
                 </div>
+                <div className="index-container">
+                    <Products />
+                </div>
             </div>
-            <div> {/* Adjusted for centering and responsiveness */}
+            <div>
                 {/* <Scene /> */}
             </div>
         </div>
